5b3bf37e280a26b1876e3adc01ff17ee22be2bc5
Christian Panici <47305647+cpanici@users.noreply.github.com>
2019-05-17 09:48:52 -0500

    Update warp animation.py

--- warp animation.py
+++ warp animation.py
@@ -2,15 +2,19 @@ import numpy as np
 from skimage import io
 import scipy.misc as smp
 from scipy.interpolate import Rbf
+from PIL import Image, ImageDraw, ImageFont
+import imageio
 
 image = io.imread('C:/Users/lwsc2/Downloads/geoImage.jpg')
 rows, cols = image.shape[0], image.shape[1]
 #2055x2048 pixel img
 
-textfile = open('C:/Users/lwsc2/Downloads/warpFrames.txt').read().split('!')
+warpCoords = open('C:/Users/lwsc2/Downloads/warpFrames.txt').read().split('!')
+importantPoints = open('C:/Users/lwsc2/Downloads/importantPoints.txt').read().split()
 
 frameNumber = 0
-for warp in textfile:
+images = []
+for warp in warpCoords:
     print(frameNumber)
     groups = warp.split()
     list_old_x, list_old_y, list_new_x, list_new_y = ([] for i in range(4))
@@ -30,11 +34,10 @@ for warp in textfile:
     interpFunctionX = Rbf(list_new_x,list_new_y,list_old_x, function = interpType)
     interpFunctionY = Rbf(list_new_x,list_new_y,list_old_y, function = interpType)
 
-    x = 300
+    x = 100
     y = x
 
     data = np.zeros( (x, y, 3), dtype=np.uint8 )
-
 
     for i in range(0, y):
         print(i/y)
@@ -54,6 +57,21 @@ for warp in textfile:
                     color = image[unwarped_x][unwarped_y]
             data[j, i] = color
 
+    #labeling important points attempt
     img = smp.toimage( data )  # Create a PIL image
+    # if frameNumber == 30:
+    #     txt = Image.new('RGBA', (2055, 2048), (255, 255, 255, 0))
+    #     fnt = ImageFont.truetype('C:/Users/lwsc2/PycharmProjects/untitled/venv/Lib/site-packages/matplotlib/mpl-data/fonts/ttf/DejaVuSans-Bold.ttf',7)
+    #     draw = ImageDraw.Draw(img)
+    #     for place in importantPoints:
+    #         placeData = place.split(',')
+    #         labelX = float(placeData[2]) * x
+    #         labelY = (1.0 - float(placeData[1])) * y
+    #         draw.text((labelX, labelY), str(placeData[0]), font=fnt, fill=(0, 0, 0))
+
+    images.append(img)
     img.save('C:/Users/lwsc2/Downloads/map'+str(frameNumber)+'.png')
     frameNumber = frameNumber + 1
+
+# images[1].save('C:/Users/lwsc2/Downloads/out3.gif', save_all = True, append_images = images, duration = 1, loop = 0)
+# imageio.mimsave('C:/Users/lwsc2/Downloads/warpAnimationFromFile2.gif', images)
