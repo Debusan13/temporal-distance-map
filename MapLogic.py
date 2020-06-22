d1762fc519fbc05f470c3ed90e32145210f802d7
ElijahNascar <56365643+ElijahNascar@users.noreply.github.com>
2020-06-22 15:22:13 -0500

    Add files via upload

---
+++ MapLogic.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 10:58:06 2020
+
+@author: Eli Nacar
+"""
+
+import numpy as np
+import requests
+import math 
+import json
+
+#Gets the image and its bounds
+orig_lat = 33.8998
+orig_long = -97.0403
+
+KEY = "AqRZj36O-xKG58fcI123-xJGxM76_CuITqN7-hRXqAI2Kbeh2yD_vv1d7U9hMZOD"
+ZOOM = "11"
+URL = "https://dev.virtualearth.net/REST/V1/Imagery/Map/Road/" + str(orig_lat) + "%2C" + str(orig_long) + "/" + ZOOM + "?mapSize=2048,2048&dpi=Large&format=png&key=" + KEY
+
+response = requests.get(URL)
+
+#gets image
+#writes image to file name
+with open('geoImage.png', 'wb') as file:
+       file.write(response.content)
+
+#gets image metadata
+URL_M = "https://dev.virtualearth.net/REST/V1/Imagery/Map/Road/" + str(orig_lat) + "%2C" + str(orig_long) + "/" + ZOOM + "?mapSize=2048,2048&mapMetadata=1&o=json&key=" + KEY
+response = requests.get(URL_M)
+
+jsonResponse = response.json()
+#array w/ long + lat bounds
+bounds = jsonResponse['resourceSets'][0]['resources'][0]['bbox']
+
+##############################################################################################
+#Mesh based on ratio of max longitude(x-axis) and latitude(y-axis)
+
+southLat = bounds[0]
+westLong = bounds[1]
+northLat = bounds[2]
+eastLong = bounds[3]
+
+#Total Longitude and Latitude depicted on map
+latDiff = northLat - southLat
+longDiff = eastLong - westLong
+
+#Interval of movement along the euclidean plane
+latMove = latDiff * .03125
+longMove = longDiff * .03125
+
+#Creation of the mesh of points (.03125 interval excluding all 0s and 1s)
+mesh = np.zeros((961, 7), dtype=float) #style [old latitude, old longitude, travel_time, old_x, old_y, new_x, new_y]
+
+x_counter = 1
+y_counter = 1
+
+#Creates a mesh of points by going through x-axis and creating points along the y-axis
+for i in mesh:
+    i[0] = southLat + latMove * y_counter #moves north incrementally
+    i[1] = westLong + longMove * x_counter #moves east incrementally
+    i[3] = 0 + .03125 * x_counter #moves along x-axis
+    i[4] = 0 + .03125 * y_counter #moves along y-axis
+    
+    y_counter += 1
+    if (y_counter == 32):
+        x_counter += 1
+        y_counter = 1
+
+###################################################################################################
+#Calculate Travel Time for every point (minutes)
+    
+KEY = "AqRZj36O-xKG58fcI123-xJGxM76_CuITqN7-hRXqAI2Kbeh2yD_vv1d7U9hMZOD"
+URL = "https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?key=" + KEY
+
+#Time chosen by user, in PST
+time = '2020-01-21T13:00:00'
+
+#Coordinates for point with the largest travel time
+maxDistance = np.zeros([7], dtype=float)
+
+#--priming payload w/ each longitude & lattitude--
+latitude = []
+longitude = []
+for item in mesh:
+    latitude.append(item[0])
+for item in mesh:
+    longitude.append(item[1])
+
+travelTimes = []
+coords = []
+for x in range(0, 961):
+    coord = {'latitude': latitude[x], 'longitude': longitude[x]}
+    coords.append(coord)
+
+payload = {'origins': [{'latitude':orig_lat, 'longitude':orig_long}],'destinations':coords,'travelMode':'driving','timeUnit':'minute'}
+
+#--getting travel times from the origin to each destination
+    
+r = requests.post(URL, json=payload)
+json_r = r.json()
+#parses "results" out of json object
+results = json_r['resourceSets'][0]['resources'][0]['results']
+#gets "travelDuration" key out of each obj in results array
+for result in results :
+    travelTimes.append(result['travelDuration'])
+    
+for i in range(0, len(mesh)):
+    mesh[i][2] = travelTimes[i]
+    if (travelTimes[i] > maxDistance[2]):
+        maxDistance = mesh[i]
+    
+###################################################################################################
+#Calculation for imPoints
+        
+#Could be an additional implementation        
+
+###################################################################################################
+#Calculate Minute Distance
+
+largestDistance = math.sqrt((maxDistance[3] - 0.5)**2 + (maxDistance[4] - 0.5)**2)
+
+OneMinuteDistance = largestDistance/maxDistance[2]
+        
+###################################################################################################
+#Find angle to each point and move based on minute distance/travel time
+
+warpMesh = open("warpMesh.txt", "w") 
+minuteDistance = open("minuteDistance.txt", "w")
+
+for i in mesh:
+    distance = i[2] * OneMinuteDistance
+    angle = math.atan2(i[4] - .5, i[3] - .5)
+    i[5] = .5 + (distance * math.cos(angle))
+    i[6] = .5 + (distance * math.sin(angle))
+    
+    warpMesh.write("%f,%f,%f,%f\n" % (i[3], i[4], i[5], i[6]))
+    
+minuteDistance.write("%f\n" % OneMinuteDistance)
+    
+warpMesh.close()
+minuteDistance.close()
--- warpAnimation.py
+++ warpAnimation.py
@@ -9,6 +9,7 @@ import os
 from datetime import datetime
 import sys
 
+#Inputs the map resolution, ring distance in minutes, frames to render (number), whether it matches scale, which frames to render
 mapResolution = int(sys.argv[1])
 ringMinutes = int(sys.argv[2])
 frames = int(sys.argv[3])
@@ -20,15 +21,18 @@ elif sys.argv[5] == "first":
 elif sys.argv[5] == "last":
     framesToRender = [frames]
 
+#distance formula
 def dist(x1,y1,x2,y2):
     return ((x1-x2)**2+(y1-y2)**2)**0.5
 
+#l2 norm formula (distance from vector)
 def l2norm(x, y):
     if x == 0 and y == 0:
         return [0, 0]
     mag = (x**2+y**2)**0.5
     return [x/mag, y/mag]
 
+#memoize function (does things faster)
 def memoize(func):
     cache = dict()
     
@@ -41,6 +45,7 @@ def memoize(func):
     
     return memoized_func
 
+#Calculates distance of each ring in minutes(already done for our implementation)
 unit = "minute"
 convertedRings = ringMinutes
 if convertedRings%60 == 0:
@@ -50,19 +55,18 @@ if convertedRings != 1:
     unit = unit+"s"
 legendText = "Contour Scale: "+str(int(convertedRings))+" "+unit
 
+#Gets direct path to file and gets a lot of txt files
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 image = io.imread(dir_path+'/geoImage.png')
 rows, cols = image.shape[0], image.shape[1]
 
 warp = open(dir_path+'/warpMesh.txt').read()
-importantPoints = open(dir_path+'/importantPoints.txt').read().split('\n')
+#importantPoints = open(dir_path+'/importantPoints.txt').read().split('\n')
 oneMinuteDistance = float(open(dir_path+'/minuteDistance.txt').read())
-if matchScale:
-    matchMeshScale = float(open(dir_path+'/matchMeshScale.txt').read())
-else:
-    matchMeshScale = 1
+matchMeshScale = 1
 
+#Splits the warp mesh data into old_x, old_y, new_x, new_y and finds distance
 groups = warp.split()
 list_old_x, list_old_y, list_new_x, list_new_y, list_old_dist, list_new_dist = ([] for i in range(6))
 for group in groups:
@@ -74,12 +78,14 @@ for group in groups:
     list_old_dist.append(dist(list_old_x[-1],list_old_y[-1],0.5,0.5))
     list_new_dist.append(dist(list_new_x[-1],list_new_y[-1],0.5,0.5))
 
+#interpolates the distance for points not specifically written
 interpType= 'linear'
 interpFunctionOldDist = Rbf(list_new_x,list_new_y,list_old_dist, function = interpType)
 interpFunctionNewDist = Rbf(list_old_x,list_old_y,list_new_dist, function = interpType)
 
 ringDist = oneMinuteDistance*ringMinutes
 
+#Begins creating frames by shifting pixels
 frameCount = 0
 pTimeProgress = 0
 numRows = mapResolution*len(framesToRender)
@@ -90,6 +96,7 @@ for frameNumber in framesToRender:
     data = np.zeros( (mapResolution, mapResolution, 4), dtype=np.uint8 )
 
     for i in range(0, mapResolution):
+        #Progress bar
         o = float(i)/mapResolution
         timeProgress = rowsDone/numRows
         if timeProgress > pTimeProgress+0.0001:
@@ -101,6 +108,7 @@ for frameNumber in framesToRender:
             with open('time.txt', 'w') as the_file:
                 the_file.write(string)
         for j in range(0, mapResolution):
+            #Finds color at old coordinates and shifts them to new
             p = float(j)/mapResolution
             
             curDist = dist(p, o, 0.5, 0.5)
@@ -116,7 +124,7 @@ for frameNumber in framesToRender:
                 unwarped_y = normalized[1]*mapDist+0.5
                 
                 if unwarped_x < 0 or unwarped_x > 1 or unwarped_y < 0 or unwarped_y > 1:
-                    color = [255,255,255,0]
+                    color = [255,255,255,0]               
                 else:
                     newDist = interpFunctionNewDist(1-unwarped_x, unwarped_y)
                 
@@ -124,37 +132,60 @@ for frameNumber in framesToRender:
                     unwarped_y = int(unwarped_y * (cols-1))
                     
                     color = image[unwarped_x][unwarped_y]
+                    
+                    #Code we added to gets rid of city names and highway symbols (should be reimplemmented if imPoints is implemented)
+                    
+                   # while ((color[0] < 190 and color[1] < 190 and color[2] < 1790) or (color[0] < 150 and color[1] < 150 and color[2] > 130)
+                          # or (color[0] > 180 and color[1] < 130 and color[2] < 130)):
+                       # unwarped_x = (unwarped_x + random.randint(-3, 3))
+                      #  unwarped_y = unwarped_y + random.randint(-3, 3)
+                       # 
+                      #  if(unwarped_x >= 1500):
+                       #     unwarped_x = 1499
+                      #  elif(unwarped_y >= 1500):
+                      #      unwarped_y = 1499
+                      #  elif(unwarped_x < 0):
+                     #       unwarped_x = 0
+                      #  elif(unwarped_y < 0):
+                     #       unwarped_y = 0
+                        
+                        #color = image[unwarped_x][unwarped_y]
                 
                     circleDist = (newDist*(1-progress)+curDist*progress)
                     if circleDist % ringDist < 0.002:
-                        color = [(color[0]+128)/2, (color[1]+128)/2, (color[2]+128)/2, color[3]]
-           
-            data[j, i] = color
+                        color = [(color[0]+128)/2, (color[1]+128)/2, (color[2]+128)/2] #color[3]]
+
+            data[j][i][0] = color[0]
+            data[j][i][1] = color[1]
+            data[j][i][2] = color[2]
+            data[j][i][3] = 255
         rowsDone = rowsDone + 1
 
-    img = Image.fromarray(data)
+    #Writes important points(we do not have any anymore) onto the image
+    #structure is Name, old_x, old_y\n - if ever implemented
+    #img = Image.fromarray(data)
         
-    txt = Image.new('RGBA', (2055, 2048), (255, 255, 255, 0))
-    fntSize = max(int(0.02*mapResolution), 10)
-    fnt = ImageFont.truetype(dir_path+'/Roboto/Roboto-Regular.ttf', fntSize)
-    draw = ImageDraw.Draw(img)
-    for place in importantPoints:
-        placeData = place.split(',')
-        if len(placeData) >= 3:
-            p = float(placeData[1])
-            o = float(placeData[2])
-            oldDist = interpFunctionOldDist(p, o)
-            normalized = l2norm(p-0.5, o-0.5)
-            curDist = dist(p, o, 0.5, 0.5)
-            mapDist = oldDist*(1-progress)+progress*curDist
-            mapDist = mapDist*(matchMeshScale*(1-progress)+progress)
-            unwarped_x = normalized[0]*mapDist+0.5
-            unwarped_y = normalized[1]*mapDist+0.5
-            labelX = unwarped_y * mapResolution
-            labelY = (1-unwarped_x) * mapResolution
-            draw.text((labelX, labelY), str(placeData[0]), font=fnt, fill=(0, 0, 0))
-    draw.text((0, mapResolution-fntSize), legendText, font=fnt, fill=(0, 0, 0))
+    #txt = Image.new('RGBA', (2055, 2048), (255, 255, 255, 0))
+    #fntSize = max(int(0.02*mapResolution), 10)
+    #fnt = ImageFont.truetype(dir_path+'/Roboto/Roboto-Regular.ttf', fntSize)
+    #draw = ImageDraw.Draw(img)
+    #for place in importantPoints:
+        #placeData = place.split(',')
+        #if len(placeData) >= 3:
+            #p = float(placeData[1])
+            #o = float(placeData[2])
+           # oldDist = interpFunctionOldDist(p, o)
+            #normalized = l2norm(p-0.5, o-0.5)
+           # curDist = dist(p, o, 0.5, 0.5)
+           # mapDist = oldDist*(1-progress)+progress*curDist
+           # mapDist = mapDist*(matchMeshScale*(1-progress)+progress)
+           # unwarped_x = normalized[0]*mapDist+0.5
+           # unwarped_y = normalized[1]*mapDist+0.5
+          #  labelX = unwarped_y * mapResolution
+          #  labelY = (1-unwarped_x) * mapResolution
+         #   draw.text((labelX, labelY), str(placeData[0]), font=fnt, fill=(0, 0, 0))
+   # draw.text((0, mapResolution-fntSize), legendText, font=fnt, fill=(0, 0, 0))
 
-    img.save(dir_path+'/Frames/map'+str(frameNumber)+'.png')
-    frameNumber = frameNumber + 1
-    frameCount = frameCount + 1
+   # img.save(dir_path+'/Frames/map'+str(frameNumber)+'.png')
+   # frameNumber = frameNumber + 1
+   # frameCount = frameCount + 1
